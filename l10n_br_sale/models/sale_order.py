8baa913ef889a03d815acbcb7c8b72c3a3ead262
Carlos <ats@atsti.com.br>
2022-02-15 09:01:21 -0300

    Comentada função não utilizada, ainda.

--- l10n_br_sale/models/sale_order.py
+++ l10n_br_sale/models/sale_order.py
@@ -246,77 +246,76 @@ class SaleOrder(models.Model):
 
         return result
 
-    def action_invoice_create(self, grouped=False, final=False):
-        # def _create_invoices(self, grouped=False, final=False, date=None):
-        inv_ids = super().action_invoice_create(grouped=grouped, final=final)
-        # inv_ids = super()._create_invoices(grouped=grouped, final=final, date=date)
+    # def _create_invoices(self, grouped=False, final=False, date=None):
+    #     import pudb;pu.db
+    #     inv_ids = super()._create_invoices(grouped=grouped, final=final, date=date)
         
-        # In brazilian localization we need to overwrite this method
-        # because when there are a sale order line with different Document
-        # Fiscal Type the method should be create invoices separated.
-        document_type_list = []
+    #     # In brazilian localization we need to overwrite this method
+    #     # because when there are a sale order line with different Document
+    #     # Fiscal Type the method should be create invoices separated.
+    #     document_type_list = []
 
-        for invoice_id in inv_ids:
-            invoice_created_by_super = self.env[
-                'account.invoice'].browse(invoice_id)
+    #     for invoice_id in inv_ids:
+    #         invoice_created_by_super = self.env[
+    #             'account.invoice'].browse(invoice_id)
 
-            # Identify how many Document Types exist
-            for inv_line in invoice_created_by_super.invoice_line_ids:
+    #         # Identify how many Document Types exist
+    #         for inv_line in invoice_created_by_super.invoice_line_ids:
 
-                fiscal_document_type = \
-                    inv_line.fiscal_operation_line_id.get_document_type(
-                        inv_line.invoice_id.company_id)
+    #             fiscal_document_type = \
+    #                 inv_line.fiscal_operation_line_id.get_document_type(
+    #                     inv_line.invoice_id.company_id)
 
-                if fiscal_document_type.id not in document_type_list:
-                    document_type_list.append(fiscal_document_type.id)
+    #             if fiscal_document_type.id not in document_type_list:
+    #                 document_type_list.append(fiscal_document_type.id)
 
-            # Check if there more than one Document Type
-            if len(document_type_list) > 1:
+    #         # Check if there more than one Document Type
+    #         if len(document_type_list) > 1:
 
-                # Remove the First Document Type,
-                # already has Invoice created
-                invoice_created_by_super.document_type_id =\
-                    document_type_list.pop(0)
+    #             # Remove the First Document Type,
+    #             # already has Invoice created
+    #             invoice_created_by_super.document_type_id =\
+    #                 document_type_list.pop(0)
 
-                for document_type in document_type_list:
-                    document_type = self.env[
-                        'l10n_br_fiscal.document.type'].browse(document_type)
+    #             for document_type in document_type_list:
+    #                 document_type = self.env[
+    #                     'l10n_br_fiscal.document.type'].browse(document_type)
 
-                    inv_obj = self.env['account.invoice']
-                    invoices = {}
-                    references = {}
-                    invoices_origin = {}
-                    invoices_name = {}
+    #                 inv_obj = self.env['account.invoice']
+    #                 invoices = {}
+    #                 references = {}
+    #                 invoices_origin = {}
+    #                 invoices_name = {}
 
-                    for order in self:
-                        group_key = order.id if grouped else (
-                            order.partner_invoice_id.id, order.currency_id.id)
+    #                 for order in self:
+    #                     group_key = order.id if grouped else (
+    #                         order.partner_invoice_id.id, order.currency_id.id)
 
-                        if group_key not in invoices:
-                            inv_data = order.with_context(
-                                document_type_id=document_type.id
-                            )._prepare_invoice()
-                            invoice = inv_obj.create(inv_data)
-                            references[invoice] = order
-                            invoices[group_key] = invoice
-                            invoices_origin[group_key] = [invoice.origin]
-                            invoices_name[group_key] = [invoice.name]
-                            inv_ids.append(invoice.id)
-                        elif group_key in invoices:
-                            if order.name not in invoices_origin[group_key]:
-                                invoices_origin[group_key].append(order.name)
-                            if (order.client_order_ref and
-                               order.client_order_ref not in
-                               invoices_name[group_key]):
-                                invoices_name[group_key].append(
-                                    order.client_order_ref)
+    #                     if group_key not in invoices:
+    #                         inv_data = order.with_context(
+    #                             document_type_id=document_type.id
+    #                         )._prepare_invoice()
+    #                         invoice = inv_obj.create(inv_data)
+    #                         references[invoice] = order
+    #                         invoices[group_key] = invoice
+    #                         invoices_origin[group_key] = [invoice.origin]
+    #                         invoices_name[group_key] = [invoice.name]
+    #                         inv_ids.append(invoice.id)
+    #                     elif group_key in invoices:
+    #                         if order.name not in invoices_origin[group_key]:
+    #                             invoices_origin[group_key].append(order.name)
+    #                         if (order.client_order_ref and
+    #                            order.client_order_ref not in
+    #                            invoices_name[group_key]):
+    #                             invoices_name[group_key].append(
+    #                                 order.client_order_ref)
 
-                    # Update Invoice Line
-                    for inv_line in invoice_created_by_super.invoice_line_ids:
-                        fiscal_document_type = \
-                            inv_line.fiscal_operation_line_id.get_document_type(
-                                inv_line.invoice_id.company_id)
-                        if fiscal_document_type.id == document_type.id:
-                            inv_line.invoice_id = invoice.id
+    #                 # Update Invoice Line
+    #                 for inv_line in invoice_created_by_super.invoice_line_ids:
+    #                     fiscal_document_type = \
+    #                         inv_line.fiscal_operation_line_id.get_document_type(
+    #                             inv_line.invoice_id.company_id)
+    #                     if fiscal_document_type.id == document_type.id:
+    #                         inv_line.invoice_id = invoice.id
 
-        return inv_ids
+    #     return inv_ids
