d1836adadd5964089d636bd8b8d8a846da2b5627
Carlos <ats@atsti.com.br>
2022-04-12 18:27:37 -0300

    Arrumado ind_final

--- l10n_br_sale/models/sale_order.py
+++ l10n_br_sale/models/sale_order.py
@@ -186,6 +186,7 @@ class SaleOrder(models.Model):
     def _onchange_partner_id(self):
         if self.partner_id.fiscal_operation_id:
             self.fiscal_operation_id = self.partner_id.fiscal_operation_id
+        self.ind_final = self.partner_id.ind_final
 
     @api.onchange('fiscal_operation_id')
     def _onchange_fiscal_operation_id(self):
