9b5949ea000f90f540abbb06cf7e1e05f4c897d3
Renan Teixeira <git@renanteixeira.com.br>
2022-06-17 00:31:32 -0300

    [UPD] Importar XML- Nome do módulo e campos

--- import_invoice/__manifest__.py
+++ import_invoice/__manifest__.py
@@ -2,12 +2,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Importar Faturas",
+    "name": "Importar Faturas XML",
     "description": """Importar Faturas""",
     "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Lucas Avila",
-    "depends": ["base", "purchase", "account", "l10n_br"],
+    "depends": ["l10n_br_base", "purchase", "account"],
     "data": [
         "security/ir.model.access.csv",
         "views/purchase_order.xml"
