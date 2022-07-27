efad7d646770791773583ac5b11821167273a7a2
Filipe Nogueira Silveira <86435640+fnosilveira@users.noreply.github.com>
2022-07-26 21:20:02 -0300

    ADD CONSULTA CNPJ CNAE

---
+++ l10n_br_consulta_cnpj_cnae/models/cnae_sec.py
@@ -0,0 +1,23 @@
+from odoo import fields, models, _
+
+
+class CnaeSecundary(models.Model):
+    _name = "cnae.cnpj.sec"
+    _description = "Cnae Secundary"
+
+    partner_id = fields.Many2one(
+        comodel_name="res.partner",
+        string="Partner",
+        ondelete="cascade",
+    )
+
+    code = fields.Char(size=16)
+    name = fields.Text(string="Name", required=True, index=True)
+
+    _sql_constraints = [
+        (
+            "cnae_cnpj_sec_code_uniq",
+            "unique (code)",
+            _("CNAE already exists with this code !"),
+        )
+    ]
