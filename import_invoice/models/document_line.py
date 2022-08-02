21116af894c148176339b8f84db745deb6607bea
Lucas Avila <lucasavila@firecodeit.net>
2022-08-02 10:18:37 -0300

    [UPD]

--- import_invoice/models/document_line.py
+++ import_invoice/models/document_line.py
@@ -18,31 +18,32 @@ class NFeLine(spec_models.StackedModel):
 
     def _export_fields(self, xsd_fields, class_obj, export_dict):
         if class_obj._name == "nfe.40.prod":
-            vals = {
-                "nfe40_DI_prod_id": self.id,
-                "nfe40_nDI" : self.number_di,
-                "nfe40_dDI" : self.date_registration,
-                "nfe40_xLocDesemb" : self.location,
-                "nfe40_UFDesemb" : self.state_id.code,
-                "nfe40_dDesemb": self.date_release,
-                "nfe40_tpViaTransp" : self.type_import,
-                "nfe40_vAFRMM" : self.afrmm_value, 
-                "nfe40_CNPJ" : self.thirdparty_cnpj,
-                "nfe40_UFTerceiro" : self.thirdparty_state_id.code,
-                "nfe40_cExportador" : self.exporting_code,
-                "nfe40_adi": [
-                    (
-                        0, 
-                        0,
-                        {
-                            "nfe40_nAdicao": line.name,
-                            "nfe40_nSeqAdic": line.sequence_di,
-                            "nfe40_cFabricante": line.manufacturer_code,
-                            "nfe40_vDescDI": line.amount_discount,
-                            "nfe40_nDraw": line.drawback_number,
-                        }
-                    ) for line in self.di_ids
-                ]
-                }
-            self.nfe40_DI = [(0,0, vals)]
+            if self.account_line_ids.move_id.move_type == "in_invoice":
+                vals = {
+                    "nfe40_DI_prod_id": self.id,
+                    "nfe40_nDI" : self.number_di,
+                    "nfe40_dDI" : self.date_registration,
+                    "nfe40_xLocDesemb" : self.location,
+                    "nfe40_UFDesemb" : self.state_id.code,
+                    "nfe40_dDesemb": self.date_release,
+                    "nfe40_tpViaTransp" : self.type_import,
+                    "nfe40_vAFRMM" : self.afrmm_value,
+                    "nfe40_CNPJ" : self.thirdparty_cnpj,
+                    "nfe40_UFTerceiro" : self.thirdparty_state_id.code,
+                    "nfe40_cExportador" : self.exporting_code,
+                    "nfe40_adi": [
+                        (
+                            0,
+                            0,
+                            {
+                                "nfe40_nAdicao": line.name,
+                                "nfe40_nSeqAdic": line.sequence_di,
+                                "nfe40_cFabricante": line.manufacturer_code,
+                                "nfe40_vDescDI": line.amount_discount,
+                                "nfe40_nDraw": line.drawback_number,
+                            }
+                        ) for line in self.di_ids
+                    ]
+                    }
+                self.nfe40_DI = [(0,0, vals)]
         return super()._export_fields(xsd_fields, class_obj, export_dict)
