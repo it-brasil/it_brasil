f7f3e8d974e82249dc37b237f1143cafe0cd928a
Lucas Avila <lucasavila@firecodeit.net>
2022-08-12 21:21:31 -0300

    [FIX]

--- import_invoice/models/document_line.py
+++ import_invoice/models/document_line.py
@@ -19,7 +19,7 @@ class NFeLine(spec_models.StackedModel):
     def _export_fields(self, xsd_fields, class_obj, export_dict):
         if class_obj._name == "nfe.40.prod":
             if self.account_line_ids.move_id.move_type == "in_invoice":
-                vals = {
+                vals_di = {
                     "nfe40_DI_prod_id": self.id,
                     "nfe40_nDI" : self.number_di,
                     "nfe40_dDI" : self.date_registration,
@@ -32,19 +32,22 @@ class NFeLine(spec_models.StackedModel):
                     "nfe40_CNPJ" : self.thirdparty_cnpj,
                     "nfe40_UFTerceiro" : self.thirdparty_state_id.code,
                     "nfe40_cExportador" : self.exporting_code,
-                    "nfe40_adi": [
-                        (
-                            0,
-                            0,
-                            {
-                                "nfe40_nAdicao": line.name,
-                                "nfe40_nSeqAdic": line.sequence_di,
-                                "nfe40_cFabricante": line.manufacturer_code,
-                                "nfe40_vDescDI": line.amount_discount,
-                                "nfe40_nDraw": line.drawback_number,
-                            }
-                        ) for line in self.di_ids
-                    ]
+                }
+               
+                list_adi = []
+                for line in self.di_ids:
+                    vals_adi = {
+                        "nfe40_nAdicao": line.name,
+                        "nfe40_nSeqAdic": line.sequence_di,
+                        "nfe40_cFabricante": line.manufacturer_code,
+                        "nfe40_vDescDI": line.amount_discount,
+                        "nfe40_nDraw": line.drawback_number,
                     }
-                self.nfe40_DI = [(0,0, vals)]
+                    list_adi.append(self.env["nfe.40.adi"].create(vals_adi).id)
+
+                vals_di["nfe40_adi"] = [(6, 0, list_adi)]
+                        
+                obj_di = self.env["nfe.40.di"].create(vals_di).id
+                self.nfe40_DI = [(6, 0, [obj_di])]
+
         return super()._export_fields(xsd_fields, class_obj, export_dict)
