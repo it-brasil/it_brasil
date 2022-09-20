1ec2a7579dd57c404f0dfd76d7617a72e5574e0b
Renan Teixeira <git@renanteixeira.com.br>
2022-09-20 12:06:03 -0300

    Ajustes OCA sale_stock,delivery,delivery_nfe

--- l10n_br_delivery_nfe/__manifest__.py
+++ l10n_br_delivery_nfe/__manifest__.py
@@ -8,12 +8,12 @@
     "license": "AGPL-3",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-brazil",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "depends": ["l10n_br_nfe", "l10n_br_account", "l10n_br_delivery"],
     "data": [
         # Views
         "views/nfe_document_view.xml"
     ],
     "installable": True,
-    "auto_install": True,
+    "auto_install": False,
 }
