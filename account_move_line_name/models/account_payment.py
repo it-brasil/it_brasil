c90ae1c0393b4bd8189622a4ce94a82da3271508
Renan Teixeira <git@renanteixeira.com.br>
2023-02-15 20:29:02 -0300

    sync account_move_line_name

--- account_move_line_name/models/account_payment.py
+++ account_move_line_name/models/account_payment.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, models, fields, _
-from datetime import date
+from odoo import models, _
+
 
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
-
 
     def _prepare_move_line_default_vals(self, write_off_line_vals=None):
 
@@ -14,9 +13,9 @@ class AccountPayment(models.Model):
 
         line_vals_list = super()._prepare_move_line_default_vals(write_off_line_vals)
         for vals in line_vals_list:
-            if (self.ref 
+            if (self.ref
                 and "name" in vals
-                and self.ref not in vals["name"]):
+                    and self.ref not in vals["name"]):
                 vals["name"] = self.ref + ' ' + vals["name"]
                 if vals["name"] == self.ref:
                     vals["name"] = self.ref + ' ' + self.account_id.name
